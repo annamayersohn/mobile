964c9052b4b4ab1542abd92703e8ff59ad2686c8
annamayersohn <78816158+annamayersohn@users.noreply.github.com>
2021-11-04 19:52:32 -0400

    Add files via upload

---
+++ function.js
@@ -0,0 +1,19 @@
+
+function menu() {
+    var x=document.querySelector("#nav");
+    if (x.className === "nav") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav";
+    }
+}
+
+
+$(document).ready(function(){
+    $(".source").hover(function(){
+        $(".source").scrollTop(0);
+        }, function(){
+        $(".source").scrollTop(0);
+    });
+});
+
